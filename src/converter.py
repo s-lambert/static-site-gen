ea230a863ba90fea1de380fb7890a6d3762c19b0
Scott Lambert <s-lambert@live.com.au>
2024-04-14 10:07:30 +0800

    Extract images and links

--- src/converter.py
+++ src/converter.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode
 
 
@@ -25,3 +27,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         split_nodes.append(TextNode(parts[i], "text"))
     new_nodes += split_nodes
   return new_nodes
+
+def extract_markdown_images(text):
+  image_regex = r"!\[(.*?)\]\((.*?)\)"
+  return find_pairs(image_regex, text)
+
+def extract_markdown_links(text):
+  link_regex = r"\[(.*?)\]\((.*?)\)"
+  return find_pairs(link_regex, text)
+
+def find_pairs(regex, text):
+  matches = re.findall(regex, text)
+  pairs = []
+  for pair in matches:
+    pairs.append((pair[0], pair[1]))
+  return pairs
--- src/test_converter.py
+++ src/test_converter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode
-from converter import split_nodes_delimiter
+from converter import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
 
 
 class TestConverter(unittest.TestCase):
@@ -30,6 +30,22 @@ class TestConverter(unittest.TestCase):
         TextNode(" words", "text"),
       ]
     )
+    
+  def test_image_extraction(self):
+    text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+    extracted_images = extract_markdown_images(text)
+    expected = [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")]
+    self.assertEqual(extracted_images, expected)
+
+  def test_link_extraction(self):
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    extracted_links = extract_markdown_links(text)
+    expected = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
+    self.assertEqual(extracted_links, expected)
+    
+  def test_no_links(self):
+    self.assertEqual(extract_markdown_links("foo bar baz"), [])
+    self.assertEqual(extract_markdown_images("foo bar baz"), [])
     
 
 if __name__ == "__main__":
