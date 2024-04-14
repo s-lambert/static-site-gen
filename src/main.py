2460c811a8ecc48197f07b44aa4b175f40f11c71
Scott Lambert <s-lambert@live.com.au>
2024-04-14 14:07:22 +0800

    Copy all static to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
-from textnode import TextNode
+import os
+import shutil
 
-
+def copy_files(source_dir, target_dir):
+  print(f"Copying from {source_dir} to {target_dir}")
+  files = os.listdir(source_dir)
+  if not(os.path.exists(target_dir)):
+    os.mkdir(target_dir)
+  for file in files:
+    source_path = os.path.join(source_dir, file)
+    target_path = os.path.join(target_dir, file)
+    
+    if os.path.isfile(source_path):
+      print(f"Copying file {source_path} to {target_path}")
+      shutil.copy(source_path, target_path)
+    else:
+      copy_files(source_path, target_path)
+  
 
 if __name__ == "__main__":
-  test = TextNode("This is a text node", "bold", "https://www.boot.dev")
-  print(test.repr())
+  public_name = "public"
+  static_path = os.path.join(".", "static")
+  public_path = os.path.join(".", public_name)
+  copy_files(static_path, public_path)
